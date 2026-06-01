eaa98a60d9bb49b9a8b040643a13e9208f77e5bc
ibrau <ilha.monahov@yandex.ru>
2026-06-01 18:38:12 +0300

    Тесты

---
+++ src/test/java/com/example/is_123_java_Monahov/Is123JavaMonahovApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.is_123_java_Monahov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Is123JavaMonahovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
